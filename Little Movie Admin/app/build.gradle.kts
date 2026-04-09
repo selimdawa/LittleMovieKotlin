73bb052ee6d5df71f87e53a4f8dd5ab70a8781f6
selimdawa <selimdawa@gmail.com>
2026-04-10 02:34:49 +0300

    ADD minorApiLevel

--- Little Movie Admin/app/build.gradle.kts
+++ Little Movie Admin/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.flatcode.littlemovieadmin"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
