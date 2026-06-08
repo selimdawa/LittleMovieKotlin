e098014544452a22e365a7bb156eb2cc30eed3b6
selimdawa <selimdawa@gmail.com>
2026-06-09 00:13:54 +0300

    Glide Update

--- Little Movie/app/build.gradle.kts
+++ Little Movie/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.services)
-    id("kotlin-kapt")
+    alias(libs.plugins.google.ksp)
 }
 
 android {
@@ -17,31 +17,38 @@ android {
         applicationId = "com.flatcode.littlemovie"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.30"
+        versionCode = 9
+        versionName = "1.35"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    signingConfigs {
-        create("release") {
-            storeFile = file("D:\\MyProjects\\Kotlin\\Little Movie\\Little Movie\\LittleMovie.jks")
-            storePassword = "00000000"
-            keyAlias = "LittleMovie"
-            keyPassword = "00000000"
-        }
-    }
-
+    //signingConfigs {
+    //    create("release") {
+    //        storeFile = file("D:\\MyProjects\\Kotlin\\Little Movie\\Little Movie\\LittleMovie.jks")
+    //        storePassword = "00000000"
+    //        keyAlias = "LittleMovie"
+    //        keyPassword = "00000000"
+    //    }
+    //}
     buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
-            isShrinkResources = true
+        release {
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
+    //buildTypes {
+    //    getByName("release") {
+    //        signingConfig = signingConfigs.getByName("release")
+    //        isMinifyEnabled = true
+    //        isShrinkResources = true
+    //        proguardFiles(
+    //            getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+    //        )
+    //    }
+    //}
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -79,8 +86,7 @@ dependencies {
     //Image
     implementation(libs.circleimageview)                //Circle Image
     implementation(libs.glide)                          //Glide Image
-    //noinspection KaptUsageInsteadOfKsp
-    kapt(libs.compiler)                                 //Glide Compiler
+    ksp(libs.glide.ksp)                                 //Glide Compiler
     implementation(libs.material.ripple)                //Ripple Effect
     api(libs.android.image.cropper)                     //Image Crop
     implementation(libs.autoimageslider)                //Slider Show
